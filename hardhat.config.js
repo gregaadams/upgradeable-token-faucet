b29f6f30fd71d640605c1c33eef4bcd13504a08e
gregaadams <workpersonalmail@proton.me>
2026-03-09 11:45:14 -0500

    Make Sepolia config conditional for CI

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-const config = {
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
   solidity: "0.8.20",
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
+    ...(SEPOLIA_URL && PRIVATE_KEY
+      ? {
+          sepolia: {
+            url: SEPOLIA_URL,
+            accounts: [PRIVATE_KEY],
+          },
+        }
+      : {}),
   },
 };
-
-if (SEPOLIA_RPC_URL && PRIVATE_KEY) {
-  config.networks.sepolia = {
-    url: SEPOLIA_RPC_URL,
-    accounts: [PRIVATE_KEY],
-  };
-}
-
-module.exports = config;
